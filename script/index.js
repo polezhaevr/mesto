b8f94bf1832dbb6a9d0636c44e5ad1f24c60b8a3
Polezhaev Roman <mrboogie8995@gmail.com>
2023-03-19 02:51:20 +0500

    feat: add a card with template and arr , add btn delete card

--- script/index.js
+++ script/index.js
@@ -7,10 +7,38 @@ let nameInput = popup.querySelector('.popup__input-text_insert_nameinput');
 let jobInput = popup.querySelector('.popup__input-text_insert_jobinput');
 let nameTitle = document.querySelector('.profile__name-title');
 let profileText = document.querySelector('.profile__text');
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+let photoPostList = document.querySelector('.photo-post__list');
+let photoPostTemplate = document.querySelector('#photo-post-template').content;
 
 
 function popupEditOpen() {
     popup.classList.add('popup_opened');
+    console.log('deleting');
 }
 
 function popupEditClose() {
@@ -19,30 +47,50 @@ function popupEditClose() {
     jobInput.value = profileText.textContent;
 }
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function handleFormSubmit(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
 
-    // Получите значение полей jobInput и nameInput из свойства value
+function handlerFormSubmit(evt) {
+    evt.preventDefault();
+
     let editName = nameInput.value;
     let editJob = jobInput.value;
 
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-
-    // Вставьте новые значения с помощью textContent
     nameTitle.textContent = editName;
     profileText.textContent = editJob;
 
     popupEditClose();
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener('submit', handlerFormSubmit);
 editButton.addEventListener('click', popupEditOpen);
 closePopupBtn.addEventListener('click', popupEditClose);
 
 
+const initialCardsInfo = initialCards.map(item => {
+    return {
+        name: item.name,
+        link: item.link
+    };
+});
 
+function insert() {
+    initialCardsInfo.forEach(inserPhotoItem);
+};
+
+function inserPhotoItem({
+    name,
+    link
+}) {
+    const photoPostItem = photoPostTemplate.querySelector('.photo-post__item').cloneNode(true);
+
+    photoPostItem.querySelector('.photo-post__image').src = link;
+    photoPostItem.querySelector('.photo-post__text').textContent = name;
+
+    photoPostList.prepend(photoPostItem);
+
+    photoPostItem.querySelector('.photo-post__btn-trash').addEventListener('click', function () {
+        photoPostItem.remove();
+        });
+
+}
+
+insert();
